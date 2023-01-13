2b6461b2a83db3582d5ca70ee2934075bf75320a
devKiratu <dev.kiratu@gmail.com>
2023-01-13 15:49:37 +0300

    add page for viewing and editing member details

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ProfileCard from "../components/ProfileCard";
 
 function Members() {
   return (
-    <div className="container">
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
-    </div>
+    <>
+      <Link to="/members/1">See member 1</Link>
+
+      <div className="container">
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+      </div>
+    </>
   );
 }
 
--- src/pages/UpdateMember.jsx
+++ src/pages/UpdateMember.jsx
@@ -1,11 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import ProfileCard from "../components/ProfileCard";
 
 function UpdateMember() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <div>
-      UpdateMember
-      <Link to={"/"}>Go back home</Link>
+      <div className="nav-btn">
+        <Link to={"/"}>
+          <button className="btn btn-nav btn-home">Back Home</button>
+        </Link>
+        <button className="btn btn-nav" onClick={() => setShowForm(!showForm)}>
+          Edit Details
+        </button>
+      </div>
+      <div className="member-details">
+        <div>
+          <ProfileCard />
+        </div>
+        {showForm && (
+          <div className="form-container">
+            <form>
+              <div className="input-group">
+                <label htmlFor="firstname">First Name</label>
+                <input
+                  type={"text"}
+                  placeholder="first name"
+                  required
+                  id="firstname"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="lastname">Last Name</label>
+                <input
+                  type={"text"}
+                  placeholder="last name"
+                  required
+                  id="lastname"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type={"email"} placeholder="email" required id="email" />
+              </div>
+              <div className="input-group">
+                <label htmlFor="occupation">Occupation</label>
+                <input
+                  type={"text"}
+                  placeholder="Occupation"
+                  required
+                  id="occupation"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="bio">Bio</label>
+                <textarea
+                  id="bio"
+                  placeholder="...a little about yourself"
+                  style={{ resize: "none" }}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <button className="btn btn-submit">Update Details</button>
+              </div>
+            </form>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
