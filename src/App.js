2154de6702b04054b743cef87b3b2a0a0f242114
devKiratu <dev.kiratu@gmail.com>
2023-01-13 14:25:47 +0300

    add routing setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import Members from "./pages/Members";
+import UpdateMember from "./pages/UpdateMember";
 
 function App() {
   return (
     <div className="app">
-      <Members />
+      <Routes>
+        <Route path="/" element={<Members />} />
+        <Route path="/members/:id" element={<UpdateMember />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/UpdateMember.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function UpdateMember() {
+  return (
+    <div>
+      UpdateMember
+      <Link to={"/"}>Go back home</Link>
+    </div>
+  );
+}
+
+export default UpdateMember;
