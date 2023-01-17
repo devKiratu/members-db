9d0649ab4c6994c38b7094d4e26cd48eecc80ff0
devKiratu <dev.kiratu@gmail.com>
2023-01-17 22:23:23 +0300

    update api endpoint and structure data accordingly

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,8 @@
-export const url = "https://touchinspiration-0ada.restdb.io/rest/sample";
+export const baseUrl =
+  "https://us-central1-ti-reactjs-test.cloudfunctions.net/app/api";
 
 export const requestHeaders = {
   headers: {
-    "x-api-key": "63be7360969f06502871ad7f",
     "Content-type": "application/json; charset=UTF-8",
   },
 };
